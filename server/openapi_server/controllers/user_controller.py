2a00fe2eb3b82449c48604c1580ffc07c7ecba08
tschaffter <thomas.schaffter@gmail.com>
2020-11-01 09:26:29 -0800

    Add initial server codebase generated with openapi-generator

---
+++ server/openapi_server/controllers/user_controller.py
@@ -0,0 +1,41 @@
+import connexion
+import six
+
+from openapi_server.models.user import User  # noqa: E501
+from openapi_server import util
+
+
+def get_user_by_name(username, pretty_print=None, with_email=None):  # noqa: E501
+    """Get user by user name
+
+    Some description of the operation. You can use &#x60;markdown&#x60; here.  # noqa: E501
+
+    :param username: The name that needs to be fetched
+    :type username: str
+    :param pretty_print: Pretty print response
+    :type pretty_print: bool
+    :param with_email: Filter users without email
+    :type with_email: bool
+
+    :rtype: User
+    """
+    return 'do some magic!'
+
+
+def update_user(username, user, pretty_print=None):  # noqa: E501
+    """Updated user
+
+    This can only be done by the logged in user. # noqa: E501
+
+    :param username: The name that needs to be updated
+    :type username: str
+    :param user: Updated user object
+    :type user: dict | bytes
+    :param pretty_print: Pretty print response
+    :type pretty_print: bool
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        user = User.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
---
+++ server/openapi_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from openapi_server.models.challenge import Challenge
+from openapi_server.models.entity import Entity
+from openapi_server.models.health import Health
+from openapi_server.models.user import User
