b619b955448c81cbcf19409af6a95770b093304d
tschaffter <thomas.schaffter@gmail.com>
2020-12-14 21:07:20 -0800

    Complete all integration tests for Tag

--- server/openapi_server/test/integration/test_tag_controller.py
+++ server/openapi_server/test/integration/test_tag_controller.py
@@ -5,10 +5,7 @@ import unittest
 
 from flask import json
 
-# from openapi_server.dbmodels.tag import Tag as DbTag # noqa: E501
-# from openapi_server.models.error import Error  # noqa: E501
-# from openapi_server.models.page_of_tags import PageOfTags  # noqa: E501
-# from openapi_server.models.tag import Tag  # noqa: E501
+from openapi_server.dbmodels.tag import Tag as DbTag  # noqa: E501
 from openapi_server.test.integration import BaseTestCase
 from openapi_server.test.integration import util
 
@@ -18,7 +15,7 @@ class TestTagController(BaseTestCase):
 
     def setUp(self):
         util.connect_db()
-        # DbTag.objects().delete()
+        DbTag.objects().delete()
 
     def tearDown(self):
         util.disconnect_db()
@@ -46,41 +43,44 @@ class TestTagController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def delete_tag(self):
+    def test_delete_tag(self):
         """Test case for delete_tag
 
         Delete a tag
         """
+        util.create_test_tag("awesome-tag")
         headers = {
             'Accept': 'application/json',
         }
         response = self.client.open(
-            '/api/v1/tags/{tag_id}'.format(tag_id='tag_id_example'),
+            '/api/v1/tags/{tag_id}'.format(tag_id='awesome-tag'),
             method='DELETE',
             headers=headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def get_tag(self):
+    def test_get_tag(self):
         """Test case for get_tag
 
         Get a tag
         """
+        util.create_test_tag("awesome-tag")
         headers = {
             'Accept': 'application/json',
         }
         response = self.client.open(
-            '/api/v1/tags/{tag_id}'.format(tag_id='tag_id_example'),
+            '/api/v1/tags/{tag_id}'.format(tag_id='awesome-tag'),
             method='GET',
             headers=headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def list_tags(self):
+    def test_list_tags(self):
         """Test case for list_tags
 
         Get all tags
         """
+        util.create_test_tag("awesome-tag")
         query_string = [('limit', 10),
                         ('offset', 0)]
         headers = {
