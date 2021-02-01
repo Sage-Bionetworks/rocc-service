e2393bb8877db03ea26821f45e7aeacce231c029
verena <9377970+vpchung@users.noreply.github.com>
2021-02-01 12:25:20 -0800

    add user db model; fix bug

--- server/openapi_server/models/__init__.py
+++ server/openapi_server/models/__init__.py
@@ -21,6 +21,7 @@ from openapi_server.models.page_of_grants import PageOfGrants
 from openapi_server.models.page_of_organizations import PageOfOrganizations
 from openapi_server.models.page_of_persons import PageOfPersons
 from openapi_server.models.page_of_tags import PageOfTags
+from openapi_server.models.page_of_users import PageOfUsers
 from openapi_server.models.person import Person
 from openapi_server.models.person_create_request import PersonCreateRequest
 from openapi_server.models.person_create_response import PersonCreateResponse
