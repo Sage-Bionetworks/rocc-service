63eb9f87624a71778b445b0fd78a0da2c5c7118c
tschaffter <thomas.schaffter@gmail.com>
2021-08-30 13:38:44 -0700

    Restore main and healthcheck controlller

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
 import connexion
+import flask
+from mongoengine import connect
 
 from openapi_server import encoder
+from openapi_server.config import Config
+
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml', pythonic_params=True)
+
+app.add_url_rule('/ui', 'ui', lambda: flask.redirect('/api/v1/ui'))
+
+connect(
+    db=Config().db_database,
+    username=Config().db_username,
+    password=Config().db_password,
+    host=Config().db_host
+)
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Registry of Open Community Challenge API'},
-                pythonic_params=True)
-
-    app.run(port=8080)
+    app.run(port=8080, debug=True)
 
 
 if __name__ == '__main__':
