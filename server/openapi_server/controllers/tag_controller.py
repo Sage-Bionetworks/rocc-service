49ee5d3d23c74d1384eff2b28b7c1e7d162bed5c
tschaffter <thomas.schaffter@gmail.com>
2021-08-30 17:18:21 -0700

    Add tag controller

--- server/openapi_server/controllers/tag_controller.py
+++ server/openapi_server/controllers/tag_controller.py
@@ -1,27 +1,41 @@
 import connexion
-import six
+from mongoengine.errors import DoesNotExist, NotUniqueError
 
-from openapi_server.models.error import Error  # noqa: E501
-from openapi_server.models.page_of_tags import PageOfTags  # noqa: E501
-from openapi_server.models.tag import Tag  # noqa: E501
-from openapi_server.models.tag_create_request import TagCreateRequest  # noqa: E501
-from openapi_server.models.tag_create_response import TagCreateResponse  # noqa: E501
-from openapi_server import util
+from openapi_server.dbmodels.tag import Tag as DbTag
+from openapi_server.models.error import Error
+from openapi_server.models.page_of_tags import PageOfTags
+from openapi_server.models.tag import Tag
+from openapi_server.models.tag_create_response import TagCreateResponse
+from openapi_server.models.tag_create_request import TagCreateRequest
+from openapi_server.config import Config
 
 
-def create_tag(tag_create_request):  # noqa: E501
+def create_tag():  # noqa: E501
     """Create a tag
 
     Create a tag with the specified name # noqa: E501
 
-    :param tag_create_request: 
-    :type tag_create_request: dict | bytes
-
     :rtype: TagCreateResponse
     """
     if connexion.request.is_json:
-        tag_create_request = TagCreateRequest.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        try:
+            tag_create_request = TagCreateRequest.from_dict(connexion.request.get_json())  # noqa: E501
+            tag = DbTag(
+                name=tag_create_request.name
+            ).save()
+            tag_id = tag.to_dict().get("id")
+            res = TagCreateResponse(id=tag_id)
+            status = 201
+        except NotUniqueError as error:
+            status = 409
+            res = Error("Conflict", status, str(error))
+        except Exception as error:
+            status = 500
+            res = Error("Internal error", status, str(error))
+    else:
+        status = 400
+        res = Error("Bad request", status, "Missing body")
+    return res, status
 
 
 def delete_all_tags():  # noqa: E501
@@ -29,10 +43,16 @@ def delete_all_tags():  # noqa: E501
 
     Delete all tags # noqa: E501
 
-
     :rtype: object
     """
-    return 'do some magic!'
+    try:
+        DbTag.objects.delete()
+        res = {}
+        status = 200
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
 
 
 def delete_tag(tag_id):  # noqa: E501
@@ -45,7 +65,17 @@ def delete_tag(tag_id):  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    try:
+        DbTag.objects.get(id=tag_id).delete()
+        res = {}
+        status = 200
+    except DoesNotExist:
+        status = 404
+        res = Error("The specified resource was not found", status)
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
 
 
 def get_tag(tag_id):  # noqa: E501
@@ -58,7 +88,17 @@ def get_tag(tag_id):  # noqa: E501
 
     :rtype: Tag
     """
-    return 'do some magic!'
+    try:
+        db_tag = DbTag.objects.get(id=tag_id)
+        res = Tag.from_dict(db_tag.to_dict())
+        status = 200
+    except DoesNotExist:
+        status = 404
+        res = Error("The specified resource was not found", status)
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
 
 
 def list_tags(limit=None, offset=None):  # noqa: E501
@@ -73,4 +113,29 @@ def list_tags(limit=None, offset=None):  # noqa: E501
 
     :rtype: PageOfTags
     """
-    return 'do some magic!'
+    try:
+        # Get results based on limit and offset.
+        db_tags = DbTag.objects.skip(offset).limit(limit)
+        tags = [Tag.from_dict(d.to_dict()) for d in db_tags]
+        next_ = ""
+        if len(tags) == limit:
+            next_ = "%s/tags?limit=%s&offset=%s" % \
+                (Config().server_api_url, limit, offset + limit)
+
+        total = db_tags.count()
+        res = PageOfTags(
+            offset=offset,
+            limit=limit,
+            paging={
+                "next": next_
+            },
+            total_results=total,
+            tags=tags)
+        status = 200
+    except TypeError:  # TODO: may need include different exceptions for 400
+        status = 400
+        res = Error("Bad request", status)
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
--- server/openapi_server/dbmodels/tag.py
+++ server/openapi_server/dbmodels/tag.py
@@ -1,12 +1,12 @@
-from mongoengine import Document, StringField
+from bson import ObjectId
+from mongoengine import Document, ObjectIdField, StringField
 
 
 class Tag(Document):
-    id = StringField(primary_key=True)
-    description = StringField()
+    id = ObjectIdField(primary_key=True, default=ObjectId)
+    name = StringField(required=True, unique=True)
 
     def to_dict(self):
         doc = self.to_mongo().to_dict()
         doc['id'] = str(self.pk)
-        doc.pop('_id', None)
         return doc
