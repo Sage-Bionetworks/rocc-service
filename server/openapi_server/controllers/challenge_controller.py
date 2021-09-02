f225622d7dc839933f50958f2ea3396eeb0bc5fb
tschaffter <thomas.schaffter@gmail.com>
2021-09-02 10:56:18 -0700

    Add new controller for challenges

--- server/openapi_server/controllers/challenge_controller.py
+++ server/openapi_server/controllers/challenge_controller.py
@@ -1,6 +1,8 @@
 import connexion
-import six
+from mongoengine.errors import DoesNotExist, NotUniqueError
 
+from openapi_server.dbmodels.account import Account as DbAccount
+from openapi_server.dbmodels.challenge import Challenge as DbChallenge
 from openapi_server.models.challenge import Challenge  # noqa: E501
 from openapi_server.models.challenge_create_request import ChallengeCreateRequest  # noqa: E501
 from openapi_server.models.challenge_create_response import ChallengeCreateResponse  # noqa: E501
@@ -9,21 +11,45 @@ from openapi_server.models.date_range import DateRange  # noqa: E501
 from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.page_of_challenges import PageOfChallenges  # noqa: E501
 from openapi_server import util
+from openapi_server.config import Config
 
 
-def create_challenge(challenge_create_request):  # noqa: E501
+def create_challenge(account_name):  # noqa: E501
     """Add a challenge
 
     Adds a challenge # noqa: E501
 
-    :param challenge_create_request: 
-    :type challenge_create_request: dict | bytes
+    :param account_name: The name of the account that owns the challenge
+    :type account_name: str
 
     :rtype: ChallengeCreateResponse
     """
     if connexion.request.is_json:
-        challenge_create_request = ChallengeCreateRequest.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        try:
+            # TODO Catch case where account is not found
+            account = DbAccount.objects.get(login=account_name)
+            account_id = account.to_dict().get("id")
+
+            challenge_create_request = ChallengeCreateRequest.from_dict(connexion.request.get_json())  # noqa: E501
+            challenge = DbChallenge(
+              name=challenge_create_request.name,
+              description=challenge_create_request.description,
+              fullName="%s/%s" % (account_name, challenge_create_request.name),
+              ownerId=account_id
+            ).save()
+            challenge_id = challenge.to_dict().get("id")
+            res = ChallengeCreateResponse(id=challenge_id)
+            status = 201
+        except NotUniqueError as error:
+            status = 409
+            res = Error("Conflict", status, str(error))
+        except Exception as error:
+            status = 500
+            res = Error("Internal error", status, str(error))
+    else:
+        status = 400
+        res = Error("Bad request", status, "Missing body")
+    return res, status
 
 
 def delete_all_challenges():  # noqa: E501
@@ -34,7 +60,14 @@ def delete_all_challenges():  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    try:
+        DbChallenge.objects.delete()
+        res = {}
+        status = 200
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
 
 
 def delete_challenge(account_name, challenge_name):  # noqa: E501
@@ -49,7 +82,21 @@ def delete_challenge(account_name, challenge_name):  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    try:
+        # TODO Catch case where account is not found
+        account = DbAccount.objects.get(login=account_name)
+        account_id = account.to_dict().get("id")
+
+        DbChallenge.objects.get(ownerId=account_id, name=challenge_name).delete()  # noqa: E501
+        res = {}
+        status = 200
+    except DoesNotExist:
+        status = 404
+        res = Error("The specified resource was not found", status)
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
 
 
 def get_challenge(account_name, challenge_name):  # noqa: E501
@@ -64,7 +111,21 @@ def get_challenge(account_name, challenge_name):  # noqa: E501
 
     :rtype: Challenge
     """
-    return 'do some magic!'
+    try:
+        # TODO Catch case where account is not found
+        account = DbAccount.objects.get(login=account_name)
+        account_id = account.to_dict().get("id")
+
+        db_user = DbChallenge.objects.get(ownerId=account_id, name=challenge_name)  # noqa: E501
+        res = Challenge.from_dict(db_user.to_dict())
+        status = 200
+    except DoesNotExist:
+        status = 404
+        res = Error("The specified resource was not found", status)
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
 
 
 def list_challenges(limit=None, offset=None, sort=None, direction=None, search_terms=None, tag_ids=None, status=None, platform_ids=None, start_date_range=None):  # noqa: E501
@@ -93,8 +154,28 @@ def list_challenges(limit=None, offset=None, sort=None, direction=None, search_t
 
     :rtype: PageOfChallenges
     """
-    if connexion.request.is_json:
-        status = [ChallengeStatus.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
-    if connexion.request.is_json:
-        start_date_range =  DateRange.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    try:
+        db_challenges = DbChallenge.objects.skip(offset).limit(limit)
+        challenges = [Challenge.from_dict(d.to_dict()) for d in db_challenges]
+        next_ = ""
+        if len(challenges) == limit:
+            next_ = "%s/users?limit=%s&offset=%s" % \
+                (Config().server_api_url, limit, offset + limit)
+
+        total = db_challenges.count()
+        res = PageOfChallenges(
+            offset=offset,
+            limit=limit,
+            paging={
+                "next": next_
+            },
+            total_results=total,
+            challenges=challenges)
+        status = 200
+    except TypeError:  # TODO: may need include different exceptions for 400
+        status = 400
+        res = Error("Bad request", status)
+    except Exception as error:
+        status = 500
+        res = Error("Internal error", status, str(error))
+    return res, status
--- server/openapi_server/dbmodels/challenge.py
+++ server/openapi_server/dbmodels/challenge.py
@@ -2,7 +2,7 @@ from bson import ObjectId
 import datetime
 from mongoengine import Document, DateTimeField, ListField, ReferenceField, StringField, URLField, ObjectIdField  # noqa: E501
 
-# from openapi_server.dbmodels.challenge_results import ChallengeResults
+from openapi_server.dbmodels.account import Account
 from openapi_server.dbmodels.challenge_platform import ChallengePlatform
 from openapi_server.dbmodels.grant import Grant
 from openapi_server.dbmodels.organization import Organization
@@ -12,24 +12,26 @@ from openapi_server.dbmodels.tag import Tag
 
 class Challenge(Document):
     id = ObjectIdField(primary_key=True, default=ObjectId)
-    name = StringField(required=True, unique=True)
+    name = StringField(required=True)
     description = StringField(required=True)
-    summary = StringField()
-    startDate = DateTimeField()
-    endDate = DateTimeField()
-    url = URLField(required=True)
-    status = StringField(
-        required=True,
-        choices=["active", "upcoming", "completed"]  # TODO: DRY
-    )
-    tagIds = ListField(ReferenceField(Tag))
-    organizerIds = ListField(ReferenceField(Person))
-    dataProviderIds = ListField(ReferenceField(Organization))
-    grantIds = ListField(ReferenceField(Grant))
-    platformId = ReferenceField(ChallengePlatform)
-    createdAt = DateTimeField(required=True, default=datetime.datetime.now)
-    updatedAt = DateTimeField(required=True, default=datetime.datetime.now)
-    # challengeResults = EmbeddedDocumentField(ChallengeResults)
+    fullName = StringField(required=True, unique=True)
+    ownerId = ReferenceField(Account)
+
+    # summary = StringField()
+    # startDate = DateTimeField()
+    # endDate = DateTimeField()
+    # url = URLField(required=True)
+    # status = StringField(
+    #     required=True,
+    #     choices=["active", "upcoming", "completed"]  # TODO: DRY
+    # )
+    # tagIds = ListField(ReferenceField(Tag))
+    # organizerIds = ListField(ReferenceField(Person))
+    # dataProviderIds = ListField(ReferenceField(Organization))
+    # grantIds = ListField(ReferenceField(Grant))
+    # platformId = ReferenceField(ChallengePlatform)
+    # createdAt = DateTimeField(required=True, default=datetime.datetime.now)
+    # updatedAt = DateTimeField(required=True, default=datetime.datetime.now)
 
     meta = {'indexes': [
         {
@@ -42,5 +44,4 @@ class Challenge(Document):
     def to_dict(self):
         doc = self.to_mongo().to_dict()
         doc["id"] = str(self.pk)
-        doc.pop('_id', None)
         return doc
