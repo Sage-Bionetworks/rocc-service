946256a158a44559a80f67b6bdb2b591b023df9d
tschaffter <thomas.schaffter@gmail.com>
2021-09-11 20:19:58 -0700

    Can authentify against password hash

--- server/openapi_server/controllers/auth_controller.py
+++ server/openapi_server/controllers/auth_controller.py
@@ -1,21 +1,37 @@
 import connexion
-import six
+from mongoengine.errors import DoesNotExist
+from werkzeug.security import check_password_hash
 
+from openapi_server.dbmodels.user import User as DbUser  # noqa: E501
 from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.local_auth_request import LocalAuthRequest  # noqa: E501
-from openapi_server import util
+# from openapi_server import util
 
 
-def auth_local(local_auth_request):  # noqa: E501
+def auth_local():  # noqa: E501
     """Authentify a local account
 
     Authentify a local account with the specified credential # noqa: E501
 
-    :param local_auth_request: 
-    :type local_auth_request: dict | bytes
-
     :rtype: object
     """
     if connexion.request.is_json:
-        local_auth_request = LocalAuthRequest.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        try:
+            local_auth_request = LocalAuthRequest.from_dict(connexion.request.get_json())  # noqa: E501
+            db_user = DbUser.objects.get(login=local_auth_request.login)
+            if check_password_hash(db_user.passwordHash, local_auth_request.password):  # noqa: E501
+                res = {}
+                status = 200
+            else:
+                res = Error("Invalid login or password")
+                status = 401
+        except DoesNotExist:
+            status = 404
+            res = Error("The specified resource was not found", status)
+        except Exception as error:
+            status = 500
+            res = Error("Internal error", status, str(error))
+    else:
+        status = 400
+        res = Error("Bad request", status, "Missing body")
+    return res, status
