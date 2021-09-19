b98133f07a5c9d2796afe99ffd4be20236ee9284
tschaffter <thomas.schaffter@gmail.com>
2021-09-18 20:43:38 -0700

    Create readme dugin challenge creation

--- server/openapi_server/controllers/challenge_controller.py
+++ server/openapi_server/controllers/challenge_controller.py
@@ -62,6 +62,12 @@ def create_challenge(account_name):  # noqa: E501
               ownerId=account_id
             ).save()
             challenge_id = challenge.to_dict().get("id")
+
+            DbChallengeReadme(
+                text=challenge_create_request.name,
+                challengeId=challenge_id
+            ).save()
+
             res = ChallengeCreateResponse(id=challenge_id)
             status = 201
         except NotUniqueError as error:
