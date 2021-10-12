2ac0529b664e30fdbc2f505247e22e26b2bed7e2
Thomas Schaffter <thomas.schaffter@gmail.com>
2021-10-12 10:49:51 -0700

    Update DB models to be compatible with DB client (#188)
    * Update DB models to be compatible with DB client
    
    * Add DB ChallengeOrganizer.v

--- server/openapi_server/dbmodels/challenge.py
+++ server/openapi_server/dbmodels/challenge.py
@@ -1,5 +1,7 @@
 from bson import ObjectId
-from mongoengine import DateTimeField, Document, ReferenceField, StringField, ObjectIdField, URLField, ListField  # noqa: E501
+import datetime
+from mongoengine import DateTimeField, Document, ReferenceField, StringField, ObjectIdField, URLField, ListField, IntField  # noqa: E501
+# , ListField
 
 from openapi_server.dbmodels.account import Account
 from openapi_server.dbmodels.challenge_platform import ChallengePlatform
@@ -10,7 +12,7 @@ class Challenge(Document):
     name = StringField(required=True)
     displayName = StringField(min_length=3, max_length=60)
     description = StringField(required=True)
-    fullName = StringField(required=True, unique=True)
+    fullName = StringField()  # TODO restore required=True, unique=True after fixing JSON seed # noqa: E501
     ownerId = ReferenceField(Account)
     websiteUrl = URLField()
     status = StringField(
@@ -22,6 +24,9 @@ class Challenge(Document):
     platformId = ReferenceField(ChallengePlatform)
     topics = ListField(StringField(unique=True), default=[])
     doi = StringField()
+    createdAt = DateTimeField(required=True, default=datetime.datetime.now)
+    updatedAt = DateTimeField(required=True, default=datetime.datetime.now)
+    v = IntField(db_field='__v')
 
     # summary = StringField()
     # startDate = DateTimeField()
--- server/openapi_server/dbmodels/challenge_readme.py
+++ server/openapi_server/dbmodels/challenge_readme.py
@@ -1,6 +1,6 @@
 import datetime
 from bson import ObjectId
-from mongoengine import DateTimeField, Document, ObjectIdField, StringField, ReferenceField  # noqa: E501
+from mongoengine import DateTimeField, Document, ObjectIdField, StringField, ReferenceField, IntField  # noqa: E501
 
 from openapi_server.dbmodels.challenge import Challenge
 
@@ -11,6 +11,7 @@ class ChallengeReadme(Document):
     challengeId = ReferenceField(Challenge, required=True, unique=True)
     createdAt = DateTimeField(required=True, default=datetime.datetime.now)
     updatedAt = DateTimeField(required=True, default=datetime.datetime.now)
+    v = IntField(db_field='__v')
 
     def to_dict(self):
         doc = self.to_mongo().to_dict()
