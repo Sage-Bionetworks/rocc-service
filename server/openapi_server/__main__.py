99e47e96e842b160339c2bb86a81d605a50117af
tschaffter <thomas.schaffter@gmail.com>
2021-10-28 15:16:10 -0700

    Revert "Rename CloudProvider to ComputeProvider"
    This reverts commit 0d1a6958582def8953db915c9031588dc0bebc77.

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
 import connexion
+import flask
+from mongoengine import connect
 
 from openapi_server import encoder
+from openapi_server.config import config
+
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml', pythonic_params=True)
+
+app.add_url_rule('/ui', 'ui', lambda: flask.redirect('/api/v1/ui'))
+
+connect(
+    db=config.db_database,
+    username=config.db_username,
+    password=config.db_password,
+    host=config.db_host
+)
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Registry of Open Community Challenges API'},
-                pythonic_params=True)
-
-    app.run(port=8080)
+    app.run(port=8080, debug=True)
 
 
 if __name__ == '__main__':
