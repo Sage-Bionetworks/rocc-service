b306f04818df34f9262364af12ff63011a0e55d4
tschaffter <thomas.schaffter@gmail.com>
2021-10-28 14:06:53 -0700

    Rename CloudProvider to ComputeProvider

--- server/openapi_server/models/challenge_sponsor_role.py
+++ server/openapi_server/models/challenge_sponsor_role.py
@@ -19,7 +19,7 @@ class ChallengeSponsorRole(Model):
     allowed enum values
     """
     CHALLENGEORGANIZER = "ChallengeOrganizer"
-    CLOUDPROVIDER = "CloudProvider"
+    COMPUTEPROVIDER = "ComputeProvider"
     DATAPROVIDER = "DataProvider"
     FUNDER = "Funder"
     OTHER = "Other"
